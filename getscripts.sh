b620ea2aeab58fd63c4c3d2d0196b5b14b60cfb7
Rohit-PX <32072909+Rohit-PX@users.noreply.github.com>
2019-09-03 17:09:40 -0700

    Update getscripts.sh

--- getscripts.sh
+++ getscripts.sh
@@ -1,3 +1,4 @@
 echo "Downloading scripts...."
 scp root@70.0.0.94:/root/Downloads/rkbkp/ClusterScripts/storkscripts/*  /root/rkscripts/storkscripts/
+scp root@70.0.0.94:/root/Downloads/rkbkp/ClusterScripts/torpscripts/*  /root/rkscripts/torpscripts/
 scp root@70.0.0.94:/root/Downloads/rkbkp/ClusterScripts/bashrc   ~/.bashrc
