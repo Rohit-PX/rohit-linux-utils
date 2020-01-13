0560330aacf1cc21ef75a2b33e5ef40598f2b653
Rohit-PX <32072909+Rohit-PX@users.noreply.github.com>
2020-01-13 11:11:06 -0800

    Update getscripts.sh

--- getscripts.sh
+++ getscripts.sh
@@ -1,4 +1,6 @@
 echo "Downloading scripts...."
+mkdir -p /root/rkscripts/storkscripts/
+mkdir -p /root/rkscripts/torpscripts/
 scp root@70.0.0.94:/root/Downloads/rkbkp/ClusterScripts/storkscripts/*  /root/rkscripts/storkscripts/
 scp root@70.0.0.94:/root/Downloads/rkbkp/ClusterScripts/torpscripts/*  /root/rkscripts/torpscripts/
 scp root@70.0.0.94:/root/Downloads/rkbkp/ClusterScripts/bashrc   ~/.bashrc
