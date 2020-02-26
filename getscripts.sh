8bed47a1fd78651bb5d0fa6b3f397e3d74618036
Rohit-PX <32072909+Rohit-PX@users.noreply.github.com>
2020-02-26 15:51:50 -0800

    Add vim colorschemes

--- getscripts.sh
+++ getscripts.sh
@@ -4,3 +4,5 @@ mkdir -p /root/rkscripts/torpscripts/
 scp root@70.0.0.94:/root/Downloads/rkbkp/ClusterScripts/storkscripts/*  /root/rkscripts/storkscripts/
 scp root@70.0.0.94:/root/Downloads/rkbkp/ClusterScripts/torpscripts/*  /root/rkscripts/torpscripts/
 scp root@70.0.0.94:/root/Downloads/rkbkp/ClusterScripts/bashrc   ~/.bashrc
+mkdir ~/.vim
+git clone https://github.com/flazz/vim-colorschemes.git ~/.vim
